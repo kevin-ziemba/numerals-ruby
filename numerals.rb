0c96dfdc43e6d1a681a5097045e84ddfd9300bd3
Kevin Ziemba <ziembakevin@gmail.com>
2019-07-05 23:16:55 -0400

    completed roman to integer conversion

--- numerals.rb
+++ numerals.rb
@@ -31,5 +31,9 @@ class String
     ROMAN_NUMBERS = [ ['M', 1000], ['CM', 900], ['D', 500], ['CD', 400], ['C', 100], ['XC', 90],
             ['L', 50], ['XL', 40], ['X', 10], ['IX', 9], ['V', 5], ['IV', 4], ['I', 1] ]
     def roman_to_i
+        r = self.upcase
+        n = 0
+        SYMBOLS.each { |sym, val| n += val while r.sub!(/^#{sym}/, "") }
+        n
     end
 end
