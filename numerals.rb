1f99210edbb6f2fe77b84d6e4dd684f92caf9471
Kevin Ziemba <ziembakevin@gmail.com>
2019-07-05 23:16:14 -0400

    completed integer to roman conversion, created reverse dictionary

--- numerals.rb
+++ numerals.rb
@@ -17,5 +17,19 @@ class Integer
     }
   
     def to_roman
+      n = self
+      roman = ""
+      ROMAN_NUMBERS.each do |value, letter|
+        roman << letter*(n / value)
+        n = n % value
+      end
+      return roman
+    end
+end
+
+class String
+    ROMAN_NUMBERS = [ ['M', 1000], ['CM', 900], ['D', 500], ['CD', 400], ['C', 100], ['XC', 90],
+            ['L', 50], ['XL', 40], ['X', 10], ['IX', 9], ['V', 5], ['IV', 4], ['I', 1] ]
+    def roman_to_i
     end
 end
